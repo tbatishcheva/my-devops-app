1a4c92bda0c53dfb237b383bec3478f5978d4227
Tatiana Batishcheva <tat.batishcheva@gmail.com>
2024-10-18 17:17:17 +0200

    Update title

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, DevOps World!');
+  res.send('Hellllllo, DevOps World!');
 });
 
 app.listen(port, () => {
